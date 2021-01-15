ec70ccae10a5b9ab1424f3f87847a85be93fcba8
Holger Mueller <9jholger@gmail.com>
2021-01-15 11:44:11 -0600

    got date and time on dom

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
-    <h1>
-      Landing forecast here.
-    </h1>
+    <v-card class="date-time-display">
+      <v-card-title>
+        {{ todaysDate }}
+      </v-card-title>
+      <v-card-subtitle>
+        {{ timeRightNow }}
+      </v-card-subtitle>
+    </v-card>
   </div>
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
   name: "Home",
+
+  data() {
+    return {
+      todaysDate: moment().format("Do MMM YYYY"),
+      timeRightNow: moment().format("HH:MM:SS"),
+    };
+  },
 };
 </script>
+
+<style scoped>
+.date-time-display {
+  width: 55%;
+  margin: 2% auto;
+}
+</style>
