fcb9889a0c82ae99852bf7594e53ae57957f5a9e
Holger Mueller <9jholger@gmail.com>
2021-02-02 12:27:34 -0600

    some styling

--- src/components/Hourly.vue
+++ src/components/Hourly.vue
@@ -1,13 +1,53 @@
 <template>
   <div>
-    <h1>Hourly forecast appears here.</h1>
+    <v-card class="forecast-display">
+      <v-card-title> </v-card-title>
+
+      <v-card-text>
+        <v-layout class="progress">
+          <v-flex class="tet-xs-center">
+            <v-progress-circular
+              indeterminate
+              class="primary--text"
+              :width="7"
+              :size="70"
+              v-if="loading"
+            ></v-progress-circular>
+          </v-flex>
+        </v-layout>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
   name: "HourlyForecast",
+
+  beforeCreate() {
+    this.$store.dispatch("getForecast");
+  },
+
+  computed: {
+    todaysForecast() {
+      return this.$store.state.weather.todaysForecast;
+    },
+
+    loading() {
+      return this.$store.getters.loading;
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.forecast-display {
+  width: 75%;
+  margin: 7% auto;
+}
+
+.progress {
+  width: 55%;
+  margin: auto;
+}
+</style>
