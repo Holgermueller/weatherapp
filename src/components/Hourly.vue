05f469e1c5356e02ac306afdbda5a4fce175deb2
Holger Mueller <9jholger@gmail.com>
2021-02-04 16:30:19 -0600

    began building hourly forecast

--- src/components/Hourly.vue
+++ src/components/Hourly.vue
@@ -1,26 +1,49 @@
 <template>
   <div>
-    <v-card class="forecast-display">
-      <v-card-title> </v-card-title>
+    <v-layout class="progress">
+      <v-flex class="tet-xs-center">
+        <v-progress-circular
+          indeterminate
+          class="primary--text"
+          :width="7"
+          :size="70"
+          v-if="loading"
+        ></v-progress-circular>
+      </v-flex>
+    </v-layout>
 
-      <v-card-text>
-        <v-layout class="progress">
-          <v-flex class="tet-xs-center">
-            <v-progress-circular
-              indeterminate
-              class="primary--text"
-              :width="7"
-              :size="70"
-              v-if="loading"
-            ></v-progress-circular>
-          </v-flex>
-        </v-layout>
-      </v-card-text>
-    </v-card>
+    <div v-for="(hour, index) in todaysForecast.hourly" :key="index">
+      <v-card class="forecast-display">
+        <v-card-title>
+          {{ convertTime(hour.dt) }}
+          <v-spacer></v-spacer>
+          {{ convertKelvinToFahrenheit(hour.temp) }}&#176; F /
+          {{ convertKelvinToCelcius(hour.temp) }}&#176; C
+        </v-card-title>
+
+        <v-card-subtitle>
+          Feels like: {{ convertKelvinToFahrenheit(hour.feels_like) }}&#176; F /
+          {{ convertKelvinToCelcius(hour.feels_like) }}&#176; C</v-card-subtitle
+        >
+
+        <v-card-text>
+          <!-- <v-img
+            alt="weather-icon"
+            height="200"
+            width="200"
+            src="http://openweathermap.org/img/wn/" + {hour.weather[0].icon} + "@2x.png"
+          ></v-img> -->
+
+          {{ hour.weather[0].description }}
+        </v-card-text>
+      </v-card>
+    </div>
   </div>
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
   name: "HourlyForecast",
 
@@ -37,13 +60,27 @@ export default {
       return this.$store.getters.loading;
     },
   },
+
+  methods: {
+    convertTime(value) {
+      return moment.unix(value).format("hh:mm A");
+    },
+
+    convertKelvinToFahrenheit(value) {
+      return Math.round((parseFloat(value) - 273.15) * 1.8 + 32);
+    },
+
+    convertKelvinToCelcius(value) {
+      return Math.round(parseFloat(value) - 273.15);
+    },
+  },
 };
 </script>
 
 <style scoped>
 .forecast-display {
   width: 75%;
-  margin: 7% auto;
+  margin: 1% auto 1%;
 }
 
 .progress {
