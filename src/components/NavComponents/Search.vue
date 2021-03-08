8f98d18f08da2bba95c59e09776f5b17f0a0e782
Holger Mueller <9jholger@gmail.com>
2021-03-08 11:27:42 -0600

    worked on search dialog

--- src/components/NavComponents/Search.vue
+++ src/components/NavComponents/Search.vue
@@ -3,21 +3,41 @@
     <v-dialog v-model="dialog" width="500">
       <template v-slot:activator="{ on, attrs }">
         <v-btn v-bind="attrs" v-on="on">
+          <v-icon left>mdi-magnify</v-icon>
           Search
         </v-btn>
       </template>
 
       <v-card>
+        <v-card-title>Seach for weather elsewhere:</v-card-title>
         <v-card-text>
-          <v-text-field placeholder="Location..."></v-text-field>
+          <v-form ref="form">
+            <v-container fluid>
+              <v-layout wrap>
+                <v-flex xs12 sm12 md12 lg12 xl12>
+                  <v-text-field
+                    type="text"
+                    v-model="location"
+                    placeholder="Location..."
+                    value="location"
+                    id="location"
+                    outlined
+                    clearable
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-form>
         </v-card-text>
 
+        <v-divider></v-divider>
+
         <v-card-actions>
-          <v-btn>Cancel</v-btn>
+          <v-btn @click="dialog = false">Cancel</v-btn>
 
           <v-spacer></v-spacer>
 
-          <v-btn>Submit</v-btn>
+          <v-btn @click.prevent="searchLocation">Submit</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -36,7 +56,20 @@ export default {
   },
 
   methods: {
-    searchLocation() {},
+    searchLocation() {
+      console.log(this.location);
+
+      this.resetForm();
+      this.closeDialog();
+    },
+
+    resetForm() {
+      this.$refs.form.reset();
+    },
+
+    closeDialog() {
+      this.dialog = false;
+    },
   },
 };
 </script>
